aa140fcf4905017b54c39357c29c6bb01f529c2b
ntdinh1987 <ntdinh1987@gmail.com>
2017-07-31 11:30:34 +0700

    VNP Framework installer

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -1,7 +1,9 @@
 <?php
 
-namespace Laravel\Installer\Console;
+namespace Vatgia\Installer\Console;
 
+use Symfony\Component\Console\Input\Input;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use ZipArchive;
 use RuntimeException;
 use GuzzleHttp\Client;
@@ -16,6 +18,17 @@ use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
 class NewCommand extends Command
 {
+
+    /**
+     * @var Input
+     */
+    protected $input;
+
+    /**
+     * @var SymfonyStyle
+     */
+    protected $output;
+
     /**
      * Configure the command options.
      *
@@ -25,58 +38,72 @@ class NewCommand extends Command
     {
         $this
             ->setName('new')
-            ->setDescription('Create a new Laravel application.')
+            ->setDescription('Create a new VNP application.')
             ->addArgument('name', InputArgument::OPTIONAL)
-            ->addOption('dev', null, InputOption::VALUE_NONE, 'Installs the latest "development" release')
             ->addOption('force', null, InputOption::VALUE_NONE, 'Forces install even if the directory already exists');
     }
 
     /**
      * Execute the command.
      *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param  \Symfony\Component\Console\Input\InputInterface $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface $output
      * @return void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (! class_exists('ZipArchive')) {
+        if (!class_exists('ZipArchive')) {
             throw new RuntimeException('The Zip PHP extension is not installed. Please install it and try again.');
         }
 
-        $directory = ($input->getArgument('name')) ? getcwd().'/'.$input->getArgument('name') : getcwd();
+        $this->input = $input;
 
-        if (! $input->getOption('force')) {
+        $output = new SymfonyStyle($input, $output);
+
+        $this->output = $output;
+
+        $directory = ($input->getArgument('name')) ? getcwd() . '/' . $input->getArgument('name') : getcwd();
+
+        if (!$input->getOption('force')) {
             $this->verifyApplicationDoesntExist($directory);
         }
 
         $output->writeln('<info>Crafting application...</info>');
 
-        $version = $this->getVersion($input);
+        $output->writeln('<info>Download view...</info>');
 
-        $this->download($zipFile = $this->makeFilename(), $version)
-             ->extract($zipFile, $directory)
-             ->prepareWritableDirectories($directory, $output)
-             ->cleanUp($zipFile);
+        //Download view
+        $this->download($zipFile = $this->makeFilename())
+            ->extract($zipFile, $directory)
+            ->cleanUp($zipFile);
+
+        $this->moveAllFile($directory . '/view.git/', $directory);
+        $this->rrmdir($directory . '/view.git/');
+
+        $this->prepareWritableDirectories($directory, $output);
+
+
+        $output->writeln('<info>Download app...</info>');
+        //Download app
+        $this->downloadApp($zipFile = $this->makeFilename())
+            ->extract($zipFile, $directory . '/app')
+            ->cleanUp($zipFile);
+
+        $this->moveAllFile($directory . '/app.git/', $directory);
+        $this->rrmdir($directory . '/app.git/');
 
         $composer = $this->findComposer();
 
         $commands = [
-            $composer.' install --no-scripts',
-            $composer.' run-script post-root-package-install',
-            $composer.' run-script post-install-cmd',
-            $composer.' run-script post-create-project-cmd',
+            $composer . ' install --no-scripts',
+            $composer . ' run-script post-root-package-install',
+            //$composer . ' run-script post-install-cmd',
+            //$composer . ' run-script post-create-project-cmd',
         ];
-
-        if ($input->getOption('dev')) {
-            unset($commands[2]);
-
-            $commands[] = $composer.' run-script post-autoload-dump';
-        }
 
         if ($input->getOption('no-ansi')) {
             $commands = array_map(function ($value) {
-                return $value.' --no-ansi';
+                return $value . ' --no-ansi';
             }, $commands);
         }
 
@@ -90,13 +117,13 @@ class NewCommand extends Command
             $output->write($line);
         });
 
-        $output->writeln('<comment>Application ready! Build something amazing.</comment>');
+        $output->writeln('<comment>VNP Application ready! Build something amazing.</comment>');
     }
 
     /**
      * Verify that the application does not already exist.
      *
-     * @param  string  $directory
+     * @param  string $directory
      * @return void
      */
     protected function verifyApplicationDoesntExist($directory)
@@ -113,43 +140,47 @@ class NewCommand extends Command
      */
     protected function makeFilename()
     {
-        return getcwd().'/laravel_'.md5(time().uniqid()).'.zip';
+        return getcwd() . '/vnp_' . md5(time() . uniqid()) . '.zip';
     }
 
     /**
      * Download the temporary Zip to the given file.
      *
-     * @param  string  $zipFile
-     * @param  string  $version
+     * @param  string $zipFile
+     * @param  string $version
      * @return $this
      */
-    protected function download($zipFile, $version = 'master')
+    protected function download($zipFile)
     {
-        switch ($version) {
-            case 'develop':
-                $filename = 'latest-develop.zip';
-                break;
-            case 'master':
-                $filename = 'latest.zip';
-                break;
-        }
-
-        $response = (new Client)->get('http://cabinet.laravel.com/'.$filename);
-
+        $response = (new Client)->get('http://gitlab.hoidap.vn/vnp-framework/view/repository/archive.zip?ref=master');
         file_put_contents($zipFile, $response->getBody());
+        return $this;
+    }
 
+    /**
+     * Download the temporary Zip to the given file.
+     *
+     * @param  string $zipFile
+     * @param  string $version
+     * @return $this
+     */
+    protected function downloadApp($zipFile)
+    {
+        $response = (new Client)->get('http://gitlab.hoidap.vn/vnp-framework/app/repository/archive.zip?ref=master');
+        file_put_contents($zipFile, $response->getBody());
         return $this;
     }
 
     /**
      * Extract the Zip file into the given directory.
      *
-     * @param  string  $zipFile
-     * @param  string  $directory
+     * @param  string $zipFile
+     * @param  string $directory
      * @return $this
      */
     protected function extract($zipFile, $directory)
     {
+
         $archive = new ZipArchive;
 
         $archive->open($zipFile);
@@ -161,10 +192,54 @@ class NewCommand extends Command
         return $this;
     }
 
+
+    // Function to remove folders and files
+    protected function rrmdir($dir)
+    {
+        if (is_dir($dir)) {
+            $files = scandir($dir);
+            foreach ($files as $file)
+                if ($file != "." && $file != "..") $this->rrmdir("$dir/$file");
+            rmdir($dir);
+        } else if (file_exists($dir)) unlink($dir);
+    }
+
+    // Function to Copy folders and files
+    protected function rcopy($src, $dst)
+    {
+//        if (file_exists($dst))
+//            $this->rrmdir($dst);
+        if (is_dir($src)) {
+
+            if (!is_dir($dst)) {
+                mkdir($dst);
+            }
+
+            $files = scandir($src);
+            foreach ($files as $file)
+                if ($file != "." && $file != "..")
+                    $this->rcopy("$src/$file", "$dst/$file");
+        } else if (file_exists($src)) {
+            copy($src, $dst);
+        }
+    }
+
+    protected function moveAllFile($source, $destination)
+    {
+
+        $source = realpath($source) . '/';
+        $destination = realpath($destination) . '/';
+
+        $this->rcopy($source, $destination);
+
+        return $this;
+
+    }
+
     /**
      * Clean-up the Zip file.
      *
-     * @param  string  $zipFile
+     * @param  string $zipFile
      * @return $this
      */
     protected function cleanUp($zipFile)
@@ -179,8 +254,8 @@ class NewCommand extends Command
     /**
      * Make sure the storage and bootstrap cache directories are writable.
      *
-     * @param  string  $appDirectory
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param  string $appDirectory
+     * @param  \Symfony\Component\Console\Output\OutputInterface $output
      * @return $this
      */
     protected function prepareWritableDirectories($appDirectory, OutputInterface $output)
@@ -188,28 +263,14 @@ class NewCommand extends Command
         $filesystem = new Filesystem;
 
         try {
-            $filesystem->chmod($appDirectory.DIRECTORY_SEPARATOR."bootstrap/cache", 0755, 0000, true);
-            $filesystem->chmod($appDirectory.DIRECTORY_SEPARATOR."storage", 0755, 0000, true);
+//            $filesystem->chmod($appDirectory . DIRECTORY_SEPARATOR . "bootstrap/cache", 0755, 0000, true);
+            $filesystem->chmod($appDirectory . DIRECTORY_SEPARATOR . "ipstore", 0755, 0000, true);
+            $filesystem->chmod($appDirectory . DIRECTORY_SEPARATOR . "ipdberror", 0755, 0000, true);
         } catch (IOExceptionInterface $e) {
-            $output->writeln('<comment>You should verify that the "storage" and "bootstrap/cache" directories are writable.</comment>');
+            $output->writeln('<comment>You should verify that the "ipstore" and "ipdberror" directories are writable.</comment>');
         }
 
         return $this;
-    }
-
-    /**
-     * Get the version that should be downloaded.
-     *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @return string
-     */
-    protected function getVersion(InputInterface $input)
-    {
-        if ($input->getOption('dev')) {
-            return 'develop';
-        }
-
-        return 'master';
     }
 
     /**
@@ -219,8 +280,8 @@ class NewCommand extends Command
      */
     protected function findComposer()
     {
-        if (file_exists(getcwd().'/composer.phar')) {
-            return '"'.PHP_BINARY.'" composer.phar';
+        if (file_exists(getcwd() . '/composer.phar')) {
+            return '"' . PHP_BINARY . '" composer.phar';
         }
 
         return 'composer';
